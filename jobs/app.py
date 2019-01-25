000f8b224386add41e266e3b4582f4e75a7e411e
Kati Botar <katalin.botar@endava.com>
2019-01-25 16:32:43 +0200

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -6,8 +6,8 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 
-def open_connection(_connection):
-    connection = getattr(g, _connection, None)
+def open_connection():
+    connection = getattr(g, '_connection', None)
     if connection is None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
